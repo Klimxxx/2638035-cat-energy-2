bb95ee87d7f0ed21febb1e81d6f99f3939e17229
Klim Trotsenko <klim.trotsenko7@gmail.com>
2025-10-09 18:54:19 +0300

    added html for catalog.html

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,10 @@
-/* в этот файл добавляет скрипты*/
+const menuButton = document.querySelector('.main-nav__toggle');
+const mainNav = document.querySelector('.main-nav');
+
+mainNav.classList.add('main-nav__closed');
+
+menuButton.addEventListener('click', () => {
+  mainNav.classList.toggle('main-nav__closed');
+  mainNav.classList.toggle('main-nav__opened');
+});
+
