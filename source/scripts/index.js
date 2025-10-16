455f276ba514d3f71400cc197a39e0901b62b1a1
Klim Trotsenko <klim.trotsenko7@gmail.com>
2025-10-16 16:09:29 +0300

    added pictures for different resolutions

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,10 +1,10 @@
 const menuButton = document.querySelector('.main-nav__toggle');
 const mainNav = document.querySelector('.main-nav');
 
-mainNav.classList.add('main-nav__closed');
+mainNav.classList.add('main-nav--closed');
 
 menuButton.addEventListener('click', () => {
-  mainNav.classList.toggle('main-nav__closed');
-  mainNav.classList.toggle('main-nav__opened');
+  mainNav.classList.toggle('main-nav--closed');
+  mainNav.classList.toggle('main-nav--opened');
 });
 
