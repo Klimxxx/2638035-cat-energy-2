6d730e10716c201c0e3cead12060474d6a7d4146
Klim Trotsenko <klim.trotsenko7@gmail.com>
2025-11-03 13:30:33 +0300

    added styles for slider tablet version

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,10 +1,25 @@
 const menuButton = document.querySelector('.main-nav__toggle');
 const mainNav = document.querySelector('.main-nav');
+const sliderControl = document.querySelector('.slider__control');
+const slider = document.querySelector('.slider');
+
+let isStarted = false;
 
 mainNav.classList.add('main-nav--closed');
 
 menuButton.addEventListener('click', () => {
   mainNav.classList.toggle('main-nav--closed');
   mainNav.classList.toggle('main-nav--opened');
+});
+
+sliderControl.addEventListener('click', ()=> {
+  if (isStarted) {
+    slider.classList.toggle('slider--after');
+    slider.classList.toggle('slider--before');
+  } else {
+    isStarted = true;
+    slider.classList.add('slider--after');
+  }
+
 });
 
